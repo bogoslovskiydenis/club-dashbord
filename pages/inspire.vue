e99028bd429e84b783c3c5cafaa0000c275573cb
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-12-20 16:38:09 +0200

    1

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -3,7 +3,7 @@
     <v-col class="text-center">
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+     <hello/>
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
@@ -15,7 +15,10 @@
 </template>
 
 <script>
+import Hello from "~/pages/hello.vue";
+
 export default {
   name: 'InspirePage',
+  components: {Hello},
 }
 </script>
