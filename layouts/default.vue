082da7959ff5340b828ae40b3b742996482963ff
DesperadoUa <Lazarev-konstant@mail.ru>
2023-12-20 17:30:18 +0200

    Add admin layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,16 @@
 <template>
   <v-app dark>
-    <HeaderApp />
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <FooterApp />
   </v-app>
 </template>
 
 <script>
-import HeaderApp from "~/components/header/header-app.vue";
-import FooterApp from "~/components/footer/footer-app.vue";
 export default {
   name: 'DefaultLayout',
-  components: {HeaderApp, FooterApp},
   data() {
     return {
     }
---
+++ pages/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row justify="center" align="center">
+    <h1>Admin main page</h1>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: "admin",
+}
+</script>
