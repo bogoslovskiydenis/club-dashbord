460b4becf23162925b9791dd0fae5239cdab6bbc
DesperadoUa <Lazarev-konstant@mail.ru>
2023-12-20 17:08:40 +0200

    Add header and footer

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,7 @@
 <template>
   <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+    <v-col class="text-center test">
+      Inspare Page
     </v-col>
   </v-row>
 </template>
@@ -19,3 +11,8 @@ export default {
   name: 'InspirePage',
 }
 </script>
+<style scoped>
+  .test {
+    border: 1px solid green;
+  }
+</style>
