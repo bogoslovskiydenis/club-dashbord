87d3f6ea650598d587210172faca72e08a445dca
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-01-01 14:22:03 +0200

    add form

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,72 @@
 <template>
   <v-app dark>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <Nuxt />
     </v-main>
   </v-app>
 </template>
-
-
-
-
 
 <script>
 export default {
   name: 'DefaultLayout',
   data() {
-    return {
-    }
+    return {}
   },
 }
 </script>
+<style>
+.form_wrapper {
+  background: linear-gradient(
+    235deg,
+    #0e0c15 6.76%,
+    #110d18 49.09%,
+    #0f0e1f 93.29%
+  );
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding-top: 140px;
+  align-items: flex-start;
+}
+.input_wrapper {
+  padding-bottom: 36px;
+}
+.input {
+  background: rgba(151, 151, 151, 0.1);
+  height: 50px;
+  width: 100%;
+  padding: 8px 8px 8px 24px;
+  color: #fffef1;
+  border-radius: 40px;
+  border: none;
+}
+.forgot_password {
+  color: #6d6d6f !important;
+  text-decoration: none;
+}
+textarea:focus,
+input:focus {
+  outline: none;
+}
+.wrapper_text {
+  padding-bottom: 30px;
+}
+.btn {
+  width: 180px;
+  display: flex;
+  border-radius: 16px;
+  background: #c500cb;
+  box-shadow: 4px 80px 62px 0px rgba(0, 0, 0, 0.8);
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  color: #fffef1;
+  font-size: 12px;
+}
+.btn_wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- pages/password-recovery.vue
+++ pages/password-recovery.vue
@@ -1,17 +1,28 @@
 <template>
-  <v-row>
-    <v-col class="text-center test">
-      PasswordRecovery Page
-      <NuxtLink  no-prefetch to="/">Registration</NuxtLink> |
-      <NuxtLink  no-prefetch to="/login">Login</NuxtLink>
-    </v-col>
-  </v-row>
+  <div class="form_wrapper">
+    <FormContainer>
+      <div class="text_wrapper">Восстановление пароля</div>
+      <div class="input_wrapper">
+        <input class="input" placeholder="Введите Никнейм или Криптач ID" />
+      </div>
+      <div class="btn_wrapper">
+        <button class="btn">Отправить</button>
+      </div>
+    </FormContainer>
+  </div>
 </template>
 
 <script>
+import FormContainer from '~/components/form_container/form-container.vue'
 export default {
   name: 'PasswordRecovery',
+  components: { FormContainer },
 }
 </script>
 <style scoped>
+.text_wrapper {
+  color: #fffef1;
+  text-align: center;
+  padding-bottom: 24px;
+}
 </style>
