4a0135f8de803860c20dafccdea58f0dbe236729
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2024-01-21 17:29:29 +0200

    мини правки 3) сделать глубину в find-partners (осталось сделать красиво)

--- pages/find-partners.vue
+++ pages/find-partners.vue
@@ -7,27 +7,34 @@
       </div>
       <div class="c_container profile_tables">
         <div class="profiles_text">Данные участников сетки</div>
-        <v-container>
-          <!-- v-data-table -->
-          <v-data-table
-            :headers="headers"
-            :items="items"
-            @click:row="onRowClick"
-          >
-          </v-data-table>
-
-          <!-- Dynamic v-expansion-panel -->
-          <v-expansion-panels>
-            <v-expansion-panel v-for="item in expandedItems" :key="item.id">
-              <v-expansion-panel-header>
-                {{ item.name }}
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                {{ item.description }}
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-container>
+        <v-data-table
+          :headers="header"
+          :expanded.sync="expanded"
+          :items="userexpanded"
+          item-key="name"
+          show-expand
+          class="elevation-2"
+        >
+          <template v-slot:expanded-item="{ item }">
+            <td>
+              <p v-for="source in item.sources" :key="source.name">{{ source.name }}</p>
+            </td>
+            <td>
+              <p v-for="source in item.sources" :key="source.lvl">{{ source.lvl }}</p>
+            </td>
+            <td>
+              <p v-for="source in item.sources" :key="source.fat">{{ source.sum }}</p>
+            </td>
+          </template>
+          <!--       <template v-slot:expanded-item="{ item }">
+                  <tr v-for="source in item.sources">
+                    <td>{{source.name}}</td>
+                    <td>{{source.calories}}</td>
+                    <td>{{source.fat}}</td>
+                    <td>{{source.carbs}}</td>
+                  </tr>
+                </template> -->
+        </v-data-table>
       </div>
     </div>
   </div>
@@ -40,36 +47,97 @@ export default {
   layout: 'admin',
   data() {
     return {
-      headers: [
-        { text: 'ID', value: 'id' },
-        { text: 'Name', value: 'name' },
-        { text: 'Description', value: 'description' },
-        { text: 'Mail', value: 'mail' },
-
+      // headers: [
+      //   { text: 'ID', value: 'id' },
+      //   { text: 'Name', value: 'name' },
+      //   { text: 'Description', value: 'description' },
+      //   { text: 'Mail', value: 'mail' },
+      //
+      // ],
+      // items: [
+      //   { id: 1, name: 'Item 1', description: 'Description 1' , mail:"gooo@gmail.com" },
+      //   { id: 2, name: 'Item 2', description: 'Description 2',mail:"gooo@gmail.com" },
+      //   { id: 2, name: 'Item 3', description: 'Description 3' ,mail:"gooo@gmail.com"},
+      //   { id: 2, name: 'Item 4', description: 'Description 4',mail:"gooo@gmail.com" },
+      //
+      //   // Add more items as needed
+      // ],
+      expanded: [],
+      // groupBy:[{
+      //   key: 'name',
+      //   subInfo:"subInfo"
+      // }],
+      header: [
+        {
+          text: 'Никнейм',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+          key:"name",
+          expand: 'data-table-expand'
+        },
+        { text: 'Уровень', value: 'lvl' },
+        { text: 'Сумма', value: 'sum' },
+        { text: 'Email', value: 'email' },
       ],
-      items: [
-        { id: 1, name: 'Item 1', description: 'Description 1' , mail:"gooo@gmail.com" },
-        { id: 2, name: 'Item 2', description: 'Description 2',mail:"gooo@gmail.com" },
-        { id: 2, name: 'Item 3', description: 'Description 3' ,mail:"gooo@gmail.com"},
-        { id: 2, name: 'Item 4', description: 'Description 4',mail:"gooo@gmail.com" },
-
-        // Add more items as needed
+      userexpanded: [
+        {
+          name: 'Елтон Джон',
+          lvl: 1,
+          sum: "1000$",
+          email:"google@gmail.com",
+          sources: [
+            {
+              name: 'Коля',
+              lvl: 2,
+              sum: "1000$",
+              email:"google@gmail.com",
+            },
+            {
+              name: 'Игорь',
+              lvl: 3,
+              sum: "1000$",
+              email:"google@gmail.com",
+            },
+            {
+              name: 'Вася',
+              lvl: 4,
+              sum: "1000321$",
+              email:"google@gmail.com",
+            }
+          ]
+        },
+        {
+          name: 'Елтон Джон2',
+          lvl: 2,
+          sum: "1000$",
+          email:"google@gmail.com",
+          sources: [
+            {
+              name: 'Петя',
+              lvl: 5,
+              sum: "1000$",
+              email:"google@gmail.com",
+            }
+          ]
+        },
+        {
+          name: 'Женя',
+          lvl: 11,
+          sum: "10$",
+          email:"google@gmail.com",
+          sources: [
+            {
+              name: 'Дед Иван))',
+              lvl: 2,
+              sum: "1$",
+              email:"google@gmail.com",
+            }
+          ]
+        }
       ],
-      expandedItems: [],
-      selectedItem: null,
-    };
-  },
-  methods: {
-    onRowClick(item) {
-      // Toggle expansion for the clicked item
-      const index = this.expandedItems.findIndex((i) => i.id === item.id);
-      if (index === -1) {
-        this.expandedItems.push(item);
-      } else {
-        this.expandedItems.splice(index, 1);
-      }
     }
-  }
+  },
 }
 </script>
 
